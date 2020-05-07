f53b5cc62df0f81b3d0753532ffd2f763fc45a18
Alex Wild <alex.j.wild@gmail.com>
2020-05-07 20:18:20 +0100

    Fix linter errors

--- src/index.ts
+++ src/index.ts
@@ -14,38 +14,40 @@ import {
   TOKEN_PREFIX,
 } from './constants';
 
-type Headers = { [key: string]: string };
+// eslint-disable-next-line functional/prefer-readonly-type
+type Headers = { [key: string]: string } | null;
 
-export interface CreateLoggerOptions {
-  audience?: string;
-  credentials: {
-    type?: string;
-    project_id: string;
-    private_key_id: string;
-    private_key: string;
-    client_email: string;
-    client_id: string;
-    auth_uri?: string;
-    token_uri?: string;
-    auth_provider_x509_cert_url?: string;
-    client_x509_cert_url?: string;
+export type CreateLoggerOptions = {
+  readonly audience?: string;
+  readonly credentials: {
+    readonly type?: string;
+    readonly project_id: string;
+    readonly private_key_id: string;
+    readonly private_key: string;
+    readonly client_email: string;
+    readonly client_id: string;
+    readonly auth_uri?: string;
+    readonly token_uri?: string;
+    readonly auth_provider_x509_cert_url?: string;
+    readonly client_x509_cert_url?: string;
   };
-  level?: Level;
-  logName?: string;
-  resource?: {
-    type: string;
-    labels?: {
-      [key: string]: string;
+  readonly level?: Level;
+  readonly logName?: string;
+  readonly resource?: {
+    readonly type: string;
+    readonly labels?: {
+      readonly [key: string]: string;
     };
   };
-  subtleCrypto?: SubtleCrypto;
-  url?: string;
+  readonly subtleCrypto?: SubtleCrypto;
+  readonly url?: string;
 };
 
-export interface GcpLogger {
-  logger: Logger;
-  flush: () => Promise<void> | void;
-}
+export type GcpLogger = {
+  readonly logger: Logger & { readonly flush: () => Promise<void> | void };
+  // eslint-disable-next-line functional/no-mixed-type
+  readonly flush: () => Promise<void> | void;
+};
 
 export function createLogger({
   audience = TOKEN_AUDIENCE,
@@ -56,48 +58,62 @@ export function createLogger({
   subtleCrypto,
   url = LOGGING_URL,
 }: CreateLoggerOptions): GcpLogger {
-  async function onError(error: Error, data: string, headers: Headers) {
-    // TODO: Handle retry if token has expired (resend data)
+  async function onError(
+    error: Readonly<Error>,
+    data: string,
+    headers: Headers
+  ): Promise<void> {
     console.error(error, data, headers);
   }
-  
-  async function createToken() {
+
+  async function createToken(): Promise<string> {
     return sign(prepareSignOptions({ credentials, audience, subtleCrypto }));
   }
-  
+
   async function prepareHeaders(headers: Headers): Promise<Headers> {
+    // Danger zone! Mutating headers to share JWT between requests.
+    /* eslint-disable functional/immutable-data, no-param-reassign */
     if (!headers) {
       headers = {};
     }
-  
-    if (!headers.Authorization || isExpired(headers.Authorization.slice(TOKEN_PREFIX.length))) {
+
+    if (
+      !headers.Authorization ||
+      isExpired(headers.Authorization.slice(TOKEN_PREFIX.length))
+    ) {
       const token = await createToken();
       headers.Authorization = `${TOKEN_PREFIX}${token}`;
     }
-  
+
     return headers;
+    /* eslint-enable functional/immutable-data, no-param-reassign */
   }
 
-  async function prepareBody(data: { [key: string]: any }): Promise<string> {
-    const entries = data.map(({ ts, level: { label }, messages }: LogEvent) => ({
-      timestamp: new Date(ts).toISOString(),
-      severity: SEVERITY_MAP[label],
-      textPayload: messages.join('\n'),
-    }));
-  
+  async function prepareBody(data: {
+    readonly [key: string]: any;
+  }): Promise<string> {
+    const entries = data.map(
+      // Too lazy to implement DeepReadonly
+      // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+      ({ ts, level: { label }, messages }: Readonly<LogEvent>) => ({
+        timestamp: new Date(ts).toISOString(),
+        severity: SEVERITY_MAP[label as Level],
+        textPayload: messages.join('\n'),
+      })
+    );
+
     return JSON.stringify({
-      logName: (logName) ? logName : DEFAULT_LOG_NAME.replace('%projectId%', credentials.project_id),
+      logName: logName
+        ? logName
+        : DEFAULT_LOG_NAME.replace('%projectId%', credentials.project_id),
       resource,
       entries,
       partialSuccess: true,
-      dryRun: false
+      dryRun: false,
     });
   }
 
-  const {
-    flush,
-    send,
-  } = pinoTransmitHttp({
+  const { flush, send } = pinoTransmitHttp({
     headers: DEFAULT_HEADERS,
     onError,
     prepareHeaders,
